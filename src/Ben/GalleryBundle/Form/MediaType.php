4e3c5a353cff3c78858526f303eeae7df8297e37
beny <explikalafourier@hotmail.com>
2015-10-29 16:04:13 +0100

    Base + gallery avant optimisation avec les évenements

---
+++ src/Ben/GalleryBundle/Form/MediaType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ben\GalleryBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MediaType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('file','file')
+            ->add('Enregistrer','submit')
+           ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Ben\GalleryBundle\Entity\Media'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ben_gallerybundle_media';
+    }
+}
