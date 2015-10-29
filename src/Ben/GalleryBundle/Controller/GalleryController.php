345c7290a760220c756b098d8365f4948c9ee8a6
beny <explikalafourier@hotmail.com>
2015-10-29 11:27:12 +0100

    Base + gallery avant optimisation avec les évenements

--- src/Ben/GalleryBundle/Controller/GalleryController.php
+++ src/Ben/GalleryBundle/Controller/GalleryController.php
@@ -2,32 +2,42 @@
 
 namespace Ben\GalleryBundle\Controller;
 
+use Ben\GalleryBundle\Entity\Media;
+use Ben\GalleryBundle\Form\MediaType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class GalleryController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        $formbuilder = $this->createFormBuilder();
+        $media = new Media();
 
-        $formbuilder
-            ->add('img','file', array('label' =>'Charger une photo'))
-            ->add('Charger','submit');
+        $form = $this->createForm(new MediaType(),$media);
 
-       $form = $formbuilder->getForm();
+        if($form->handleRequest($request)->isValid()){
+
+            $media->upload();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($media);
+            $em->flush();
+
+            return $this->redirectToRoute('gallery_home');
+            }
+
+
+        //Récupération des photos
+        $repo = $this->getDoctrine()->getRepository('BenGalleryBundle:Media');
+        $photos = $repo->findAll();
 
 
         return $this->render('BenGalleryBundle:Gallery:index.html.twig',array(
             'form' => $form->createView(),
+            'media' => $media,
+            'photos' =>$photos
             ));
     }
 
-    public function addAction()
-    {
-       $formbuilder = $this->createFormBuilder();
 
-        $formbuilder
-            ->add('img','file', array('label' =>'Charger une photo'))
-            ->add('Charger','submit');
-    }
 }
